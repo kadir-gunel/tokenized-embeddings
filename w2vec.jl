2fc80e68f7d3d3e1fcd37c6a7a5d0df6b0b54a0e
kadir-gunel <7396849+kadir-gunel@users.noreply.github.com>
2025-05-14 23:10:34 +0300

    faster word2vec

--- w2vec.jl
+++ w2vec.jl
@@ -1,6 +1,139 @@
-using Flux, Flux.Zygote, Flux.Optimise
+using Statistics
 using LinearAlgebra
+
+using Pkg
+Pkg.activate("/home/kguenel/Glove")
+
+
+using NNlib
+using Flux
+using Zygote
+using Optimisers
+
 using Flux: @layer
+
+
+# Tokenize and prepare data
+function preprocess(corpus)
+    words = split(lowercase(corpus))
+    vocab = Dict(word => i for (i, word) in enumerate(unique(words)))
+    idx_to_word = [word for (word, _) in sort(collect(vocab), by = x -> x[2])]
+    word_indices = [vocab[word] for word in words]
+    return word_indices, vocab, idx_to_word
+end
+
+# Generate skip-gram pairs
+function generate_pairs(word_indices, window_size=2)
+    pairs = []
+    for i in 1:length(word_indices)
+        for j in max(i - window_size, 1):min(i + window_size, length(word_indices))
+            if i != j
+                push!(pairs, (word_indices[i], word_indices[j]))
+            end
+        end
+    end
+    return pairs
+end
+
+# Negative sampling: generate n negative samples not equal to the positive context word
+function get_negative_samples(target, vocab_size, n)
+    samples = Int[]
+    while length(samples) < n
+        neg = rand(1:vocab_size)
+        if neg != target
+            push!(samples, neg)
+        end
+    end
+    return samples
+end
+
+get_negative_samples(contexts, vocab_size, k) = rand(1:vocab_size, k, length(contexts)) # k,b
+
+
+struct Word2Vec
+    V::Embedding
+    U::Embedding    
+end
+@layer Word2Vec
+
+function Word2Vec(VSIZE::Int, IN_DIM::Int)
+    V = Embedding(VSIZE, IN_DIM)
+    U = Embedding(VSIZE, IN_DIM)
+    return Word2Vec(V, U)
+end
+
+
+(m::Word2Vec)(center, context, neg_samples) = m.V(center), m.U(context), m.U(neg_samples)
+
+function loss(model::Word2Vec, center::Vector{Int}, context::Vector{Int}, neg_samples::Matrix{Int})
+
+    # DxB,   DxB,     DxKxB  
+    inputs, posouts, negouts = model(center, context, neg_samples)
+
+    pos_scores = sum(log.(sigmoid.(sum(dot(inputs, posouts))))) # scalar
+
+    # need to reshape the inputs
+    dsize, bsize = inputs |> size
+    inputs = reshape(inputs, 1, dsize, bsize) # (D, 1, B)
+
+    neg_scores = sum(log.(sigmoid.(-batched_mul(inputs, negouts))))
+
+    return (pos_scores + neg_scores) / bsize
+end
+
+
+# Forward + loss (negative sampling)
+function loss_fn(model::Word2Vec, center, context, negative_samples)
+    v_c = model.input_embeddings[:, center]
+    v_o = model.output_embeddings[:, context]
+
+    # Positive score
+    pos_score = log(sigmoid(dot(v_c, v_o)))
+
+    # Negative score
+    neg_score = 0.0
+    for neg in negative_samples
+        v_n = model.output_embeddings[:, neg]
+        neg_score += log(sigmoid(-dot(v_c, v_n)))
+    end
+
+    return - (pos_score + neg_score)
+end
+
+# Training loop
+function train!(model::Word2Vec, pairs, vocab_size; epochs=10, lr=0.01, neg_samples=5)
+    opt = Descent(lr)
+    for epoch in 1:epochs
+        total_loss = 0.0
+        for (center, context) in pairs
+            negative_samples = get_negative_samples(context, vocab_size, neg_samples)
+            gs = Flux.gradient(() -> loss_fn(model, center, context, negative_samples),
+                               Flux.params(model.input_embeddings, model.output_embeddings))
+            Flux.Optimise.update!(opt, Flux.params(model.input_embeddings, model.output_embeddings), gs)
+            total_loss += loss_fn(model, center, context, negative_samples)
+        end
+        println("Epoch $epoch, Loss = $(total_loss / length(pairs))")
+    end
+end
+
+# Example usage
+corpus = "the quick brown fox jumps over the lazy dog"
+word_indices, vocab, idx_to_word = preprocess(corpus)
+pairs = generate_pairs(word_indices, 2)
+vocab_size = length(vocab)
+embed_dim = 10
+model = Word2Vec(vocab_size, embed_dim)
+train!(model, pairs, vocab_size, epochs=100, lr=0.05)
+
+# Getting embeddings
+embedding_matrix = model.input_embeddings
+
+
+
+
+
+
+"""
 
 struct Embedding{T}
     W::T
@@ -62,101 +195,5 @@ embedding_size = 16
 #  end
 
 # end
+"""
 
-
-
-using Flux
-using Random
-using Statistics
-
-# Tokenize and prepare data
-function preprocess(corpus)
-    words = split(lowercase(corpus))
-    vocab = Dict(word => i for (i, word) in enumerate(unique(words)))
-    idx_to_word = [word for (word, _) in sort(collect(vocab), by = x -> x[2])]
-    word_indices = [vocab[word] for word in words]
-    return word_indices, vocab, idx_to_word
-end
-
-# Generate skip-gram pairs
-function generate_pairs(word_indices, window_size=2)
-    pairs = []
-    for i in 1:length(word_indices)
-        for j in max(i - window_size, 1):min(i + window_size, length(word_indices))
-            if i != j
-                push!(pairs, (word_indices[i], word_indices[j]))
-            end
-        end
-    end
-    return pairs
-end
-
-# Negative sampling: generate n negative samples not equal to the positive context word
-function get_negative_samples(target, vocab_size, n)
-    samples = Int[]
-    while length(samples) < n
-        neg = rand(1:vocab_size)
-        if neg != target
-            push!(samples, neg)
-        end
-    end
-    return samples
-end
-
-# Word2Vec model
-struct Word2Vec
-    input_embeddings::Matrix{Float32}
-    output_embeddings::Matrix{Float32}
-end
-
-function Word2Vec(vocab_size::Int, embed_dim::Int)
-    input_embeddings = rand(Float32, embed_dim, vocab_size)
-    output_embeddings = rand(Float32, embed_dim, vocab_size)
-    return Word2Vec(input_embeddings, output_embeddings)
-end
-
-# Forward + loss (negative sampling)
-function loss_fn(model::Word2Vec, center, context, negative_samples)
-    v_c = model.input_embeddings[:, center]
-    v_o = model.output_embeddings[:, context]
-
-    # Positive score
-    pos_score = log(sigmoid(dot(v_c, v_o)))
-
-    # Negative score
-    neg_score = 0.0
-    for neg in negative_samples
-        v_n = model.output_embeddings[:, neg]
-        neg_score += log(sigmoid(-dot(v_c, v_n)))
-    end
-
-    return - (pos_score + neg_score)
-end
-
-# Training loop
-function train!(model::Word2Vec, pairs, vocab_size, epochs=10, lr=0.01, neg_samples=5)
-    opt = Descent(lr)
-    for epoch in 1:epochs
-        total_loss = 0.0
-        for (center, context) in pairs
-            negative_samples = get_negative_samples(context, vocab_size, neg_samples)
-            gs = Flux.gradient(() -> loss_fn(model, center, context, negative_samples),
-                               Flux.params(model.input_embeddings, model.output_embeddings))
-            Flux.Optimise.update!(opt, Flux.params(model.input_embeddings, model.output_embeddings), gs)
-            total_loss += loss_fn(model, center, context, negative_samples)
-        end
-        println("Epoch $epoch, Loss = $(total_loss / length(pairs))")
-    end
-end
-
-# Example usage
-corpus = "the quick brown fox jumps over the lazy dog"
-word_indices, vocab, idx_to_word = preprocess(corpus)
-pairs = generate_pairs(word_indices, 2)
-vocab_size = length(vocab)
-embed_dim = 10
-model = Word2Vec(vocab_size, embed_dim)
-train!(model, pairs, vocab_size, epochs=100, lr=0.05)
-
-# Getting embeddings
-embedding_matrix = model.input_embeddings
